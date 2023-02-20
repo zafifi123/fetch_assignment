a19b67f271bcd6a318841d6088b4ca5ac9131aa1
zafifi123 <125923397+zafifi123@users.noreply.github.com>
2023-02-20 15:46:26 -0600

    Update and rename mycode.java to Spend.java

--- Spend.java
+++ Spend.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class SpendPoints {
+public class Spend{
    
     private static List<Transaction> readTransactionsFromFile(String filename) {
         List<Transaction> transactions = new ArrayList<>();
